d4eaa8b71bfe0545939e484c7f79e2b4641b59d7
.ursys <jebs.junkmail@gmail.com>
2015-05-07 22:58:46 -0400

    adding demo website

---
+++ AislingBrew/script.js
@@ -0,0 +1,9 @@
+document.getElementById("foot01").innerHTML = 
+"<p>&copy; " + new Date().getFullYear() + "Aisling Homebrew Project. All rights reserved.</p>";
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='beers.html'>Our Beers</a></li>" +
+"<li><a href='about.html'>Our Story</a></li>" +
+"</ul>";
